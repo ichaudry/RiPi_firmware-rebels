105787efaf8c466df32a8249211564b65cba9a19
ichaudry <ibraheem27496@gmail.com>
2020-04-15 16:58:12 -0700

    Attempting to fix garbage value errors.

--- src/infraredSensor.c
+++ src/infraredSensor.c
@@ -4,17 +4,16 @@
 #include <ctype.h>
 #include "infraredSensor.h"
 
+    //The number of pulses in one revolution
+    double * N=20;
 
-   void initializeIR() {
+    void initializeIR() {
        pinMode(IR_SENSOR,INPUT);
-   }
+    }
 
-   void * getSpeed() {
+    void * getSpeed() {
        //Number of pulses
        double n=0;
-
-       //The number of pulses in one revolution
-       double N=20;
 
        int count=0;
 
@@ -38,11 +37,12 @@
 
        double time_taken = ((double)t)/((double)CLOCKS_PER_SEC); // in seconds
 
-       printf("The time taken for the function is %f\nThe PI value is %f\nThe value of n is %lu\nThe value of N is %d\n",time_taken,PI,n,N);
+       printf("The time taken for the function is %f\nThe PI value is %f\nThe value of n is %lu\nThe value of N is %d\n",time_taken,PI,n,&N);
 
        double angularSpeed= (2* PI * n )/(N * time_taken);
 
        printf("The angular speed is %lu\n",angularSpeed);
+
 
    }
 
